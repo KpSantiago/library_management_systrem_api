3a8959ac669b5c3d31b61561636f69bcdbdb1319
KpSantiago <santiago.kauajose11@gmail.com>
2024-01-11 10:12:43 -0300

    updates added

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const app = express();
 
 require('./database');
 
-app.listen(process.env.PORT | 3333, () => console.log('listen on port 3333'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 app.use(routes);
+
+app.listen(process.env.PORT | 3333, () => console.log('listen on port 3333'));
 
 app.use('/api/image/', express.static(path.resolve(__dirname, 'images', 'uploads')))
